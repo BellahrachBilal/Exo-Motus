7d67975a5fb7bf66a4ccc324df5b932e2019a206
Adrien Legrain <skynico200@gmail.com>
2024-09-24 15:45:21 +0200

    Ajout methode affichage Adrien

--- Exo-Motus/Program.cs
+++ Exo-Motus/Program.cs
@@ -4,7 +4,39 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+        }
+        
+        public static string[] Show(string[] tabMot, int[] tabValeur) // Verification + attribution de couleur en fonction des valeurs reprisent du tableau de int
+        {
+            string[] result = new string[tabMot.Length]; 
+
+            for (int i = 0; i < tabMot.Length; i++)
+            {
+                if (tabValeur[i] == 2)
+                {
+                    result[i] = $"\u001b[31m{tabMot[i]}\u001b[0m"; // Attribution de la couleur rouge car bonne réponse
+                }
+                else if (tabValeur[i] == 1)
+                {
+                    result[i] = $"\u001b[33m{tabMot[i]}\u001b[0m"; // Attribution de la couleur jaune car bonne lettre mais mal placée
+                }
+                else
+                {
+                    result[i] = $"{tabMot[i]}"; // rien ne se passe car tout faux
+                }
+            }
+
+                return result;
+        }
+        public static void ShowResult(string[] tabMot) // Méthode d'affichage reprennant le tableau résult de la méthode Show.
+        {
+
+            Console.WriteLine(" _____________________________ ");
+            Console.WriteLine("|     |     |     |     |     |");
+            Console.WriteLine($"|  {tabMot[0]}  |  {tabMot[1]}  |  {tabMot[2]}  |  {tabMot[3]}  |  {tabMot[4]}  |");
+            Console.WriteLine("|     |     |     |     |     |");
+            Console.WriteLine(" ----------------------------- ");
         }
     }
 }
