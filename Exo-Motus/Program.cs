9adf6ad0f13638a176c4af5785aa2a156b09d1a6
Raphaël Marto <divinitinatsu@outlook.fr>
2024-09-24 15:57:30 +0200

    Verif valid caractere wrong place

--- Exo-Motus/Program.cs
+++ Exo-Motus/Program.cs
@@ -4,7 +4,42 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Dictionary<char, int> NumberLetterOccurence(string Word, int[] RightPlace){
+                Dictionary<char, int> letterCounts = new Dictionary<char, int>();
+                for(int i=0;i<Word.Length;i++){
+                    if(RightPlace[i] != 1)
+                    {
+                        if (letterCounts.ContainsKey(Word[i]))
+                        {
+                            letterCounts[Word[i]]++;
+                        }
+                        else
+                        {
+                            letterCounts[Word[i]] = 1;
+                        }
+                    }                    
+                }
+                return letterCounts;
+            }
+
+            int[] RightCaractInWordWrongPlace(string Word, int[] RightPlace, string RightWord)
+            {
+                Dictionary<char, int> letterCounts = NumberLetterOccurence(RightWord, RightPlace);
+
+                for(int i = 0; i<Word.Length; i++){
+                    for(int y =0; y<Word.Length; y++)
+                    {
+                        if(Word[i] == RightWord[y] && RightPlace[i] != 1){
+                            if(letterCounts[RightWord[y]]>0)
+                            {
+                                RightPlace[i] = 2;
+                                letterCounts[RightWord[y]]--;
+                            }                            
+                        }
+                    }
+                }
+                return RightPlace;                      
+            }
         }
     }
 }
