c1d2ee4319dd419e5898d2fe48088fb0248d8cfe
Raphaël Marto <divinitinatsu@outlook.fr>
2024-09-24 16:03:15 +0200

    Fix Funct Pose

--- Exo-Motus/Program.cs
+++ Exo-Motus/Program.cs
@@ -4,42 +4,45 @@
     {
         static void Main(string[] args)
         {
-            Dictionary<char, int> NumberLetterOccurence(string Word, int[] RightPlace){
-                Dictionary<char, int> letterCounts = new Dictionary<char, int>();
-                for(int i=0;i<Word.Length;i++){
-                    if(RightPlace[i] != 1)
+           
+        }
+
+        public Dictionary<char, int> NumberLetterOccurence(string Word, int[] RightPlace)
+        {
+            Dictionary<char, int> letterCounts = new Dictionary<char, int>();
+            for(int i=0;i<Word.Length;i++){
+                if(RightPlace[i] != 1)
+                {
+                    if (letterCounts.ContainsKey(Word[i]))
                     {
-                        if (letterCounts.ContainsKey(Word[i]))
-                        {
-                            letterCounts[Word[i]]++;
-                        }
-                        else
-                        {
-                            letterCounts[Word[i]] = 1;
-                        }
-                    }                    
-                }
-                return letterCounts;
+                        letterCounts[Word[i]]++;
+                    }
+                    else
+                    {
+                        letterCounts[Word[i]] = 1;
+                    }
+                }                    
             }
+            return letterCounts;
+        }
 
-            int[] RightCaractInWordWrongPlace(string Word, int[] RightPlace, string RightWord)
-            {
-                Dictionary<char, int> letterCounts = NumberLetterOccurence(RightWord, RightPlace);
+        public int[] RightCaractInWordWrongPlace(string Word, int[] RightPlace, string RightWord)
+        {
+            Dictionary<char, int> letterCounts = NumberLetterOccurence(RightWord, RightPlace);
 
-                for(int i = 0; i<Word.Length; i++){
-                    for(int y =0; y<Word.Length; y++)
-                    {
-                        if(Word[i] == RightWord[y] && RightPlace[i] != 1){
-                            if(letterCounts[RightWord[y]]>0)
-                            {
-                                RightPlace[i] = 2;
-                                letterCounts[RightWord[y]]--;
-                            }                            
-                        }
+            for(int i = 0; i<Word.Length; i++){
+                for(int y =0; y<Word.Length; y++)
+                {
+                    if(Word[i] == RightWord[y] && RightPlace[i] != 1){
+                        if(letterCounts[RightWord[y]]>0)
+                        {
+                            RightPlace[i] = 2;
+                            letterCounts[RightWord[y]]--;
+                        }                            
                     }
                 }
-                return RightPlace;                      
             }
+            return RightPlace;                      
         }
     }
 }
