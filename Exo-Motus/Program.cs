d3e221ffffab61fe0daec48192e28a6e33f85ddf
Scorniciel Ayrton <scorniciel.ayrton@hotmail.com>
2024-09-24 16:29:45 +0200

    Ajout RightCaracInWordRightPlace

--- Exo-Motus/Program.cs
+++ Exo-Motus/Program.cs
@@ -1,10 +1,51 @@
-﻿namespace Exo_Motus
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Exo_Motus
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+        }
+        public Dictionary<char, int> NumberLetterOccurence(string Word, int[] RightPlace)
+        {
+            Dictionary<char, int> letterCounts = new Dictionary<char, int>();
+            for (int i = 0; i < Word.Length; i++)
+            {
+                if (RightPlace[i] != 1)
+                {
+                    if (letterCounts.ContainsKey(Word[i]))
+                    {
+                        letterCounts[Word[i]]++;
+                    }
+                    else
+                    {
+                        letterCounts[Word[i]] = 1;
+                    }
+                }
+            }
+            return letterCounts;
+        }
+        public int[] RightCaracInWordRightPlace(string Word, int[] RightPlace, string RightWord)
+        {
+            Dictionary<char, int> lettersCounts = NumberLetterOccurence(RightWord, RightPlace);
+
+            for (int i = 0; i < Word.Length; i++)
+            {
+                for (int y = 0; y < RightWord.Length; y++)
+                {
+                    if (Word[i] == RightWord[y] && RightPlace[i] == 1)
+                    {
+                        if (lettersCounts[RightWord[y]] > 0)
+                        {
+                            RightPlace[i] = 1;
+                            lettersCounts[RightWord[y]]--;
+                        }
+                    }
+                }
+            }
+            return RightPlace;
         }
     }
 }
