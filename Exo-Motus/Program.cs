18357c52f4d3f911b5654f9a48567b6fe5efb980
Raphaël Marto <divinitinatsu@outlook.fr>
2024-09-24 16:50:46 +0200

    Fix Algo

--- Exo-Motus/Program.cs
+++ Exo-Motus/Program.cs
@@ -8,37 +8,14 @@ namespace Exo_Motus
         {
 
         }
-        public Dictionary<char, int> NumberLetterOccurence(string Word, int[] RightPlace)
-        {
-            Dictionary<char, int> letterCounts = new Dictionary<char, int>();
-            for (int i = 0; i < Word.Length; i++)
-            {
-                if (RightPlace[i] != 1)
-                {
-                    if (letterCounts.ContainsKey(Word[i]))
-                    {
-                        letterCounts[Word[i]]++;
-                    }
-                }
-            }
-            return letterCounts;
-        }
         public int[] RightCaracInWordRightPlace(string Word, int[] RightPlace, string RightWord)
         {
-            Dictionary<char, int> lettersCounts = NumberLetterOccurence(RightWord, RightPlace);
-
             for (int i = 0; i < Word.Length; i++)
             {
-                for (int y = 0; y < RightWord.Length; y++)
-                {
-                    if (Word[i] == RightWord[y] && RightPlace[i] == 1)
-                    {
-                        if (lettersCounts[RightWord[y]] > 0)
-                        {
-                            lettersCounts[RightWord[y]]--;
-                        }
-                    }
-                }
+                if (Word[i] == RightWord[i])
+                {                   
+                    RightPlace[i] = 1;                       
+                } 
             }
             return RightPlace;
         }
